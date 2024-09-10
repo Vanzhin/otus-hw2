5802c055a5f8bcde3a60fe7a43bc0040fae66fbb
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2024-09-10 14:21:39 +0500

    hw6.feat.update headers for route token check

--- docker/app/src/Share/Infrastructure/Controller/TokenCheckAction.php
+++ docker/app/src/Share/Infrastructure/Controller/TokenCheckAction.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace App\Share\Infrastructure\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api/auth/token/validate', name: 'api_token_check', methods: ['GET'])]
-class TokenCheckAction
+class TokenCheckAction extends AbstractController
 {
     public function __invoke(): JsonResponse
     {
-        return new JsonResponse();
+        $response = new JsonResponse();
+        $response->headers->add(['X-User' => $this->getUser()->getUlid()]);
+        return $response;
     }
 }
